a7654f4ad9aebae505a9b6844f1460daf32f3155
ShushanGevorgyan1993 <gevorgyanshushan.93@gmail.com>
2025-06-14 12:44:38 +0400

    added fileWriter

--- src/main/java/am/order/homeworkForMap/Task2.java
+++ src/main/java/am/order/homeworkForMap/Task2.java
@@ -1,9 +1,15 @@
 package am.order.homeworkForMap;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter writer = new FileWriter("phoneBook.txt", true);
+        BufferedWriter saveContact = new BufferedWriter(writer);
 
         PhoneBook contact = new PhoneBook();
 
@@ -26,6 +32,9 @@ public class Task2 {
                     System.out.println("Type number");
                     String number = scanner.next();
                     contact.addContact(name, number);
+                    saveContact.write(name + ": " + number);
+                    saveContact.newLine();
+                    saveContact.flush();
                     System.out.println("contact added");
                     break;
                 case "2":
@@ -42,8 +51,9 @@ public class Task2 {
                     break;
             }
         }
+
+        saveContact.close();
     }
-
-
 }
+
 
